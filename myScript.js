c8cf70fc02a1ccb672f01ce466e90c438eadb17c
MariaLinhares <marialuis.linhares10@gmail.com>
2022-12-28 20:30:16 +0000

    Fixed countries cards

--- myScript.js
+++ myScript.js
@@ -6,14 +6,16 @@ $('document').ready(function () {
 
     const api_url = "http://192.168.160.58/Olympics/Help"
 
-    $("#searchbar").autocomplete({
-        minLenght: 3,
-        source: function (request, response) {
-            $.ajax({
-                type: "GET",
-                url: api_url,
-                data: {
-                    name: $('#searchbar').val().toLowerCase()
-                },
-    });
-});
+    // $("#searchbar").autocomplete({
+    //     minLenght: 3,
+    //     source: function (request, response) {
+    //         $.ajax({
+    //             type: "GET",
+    //             url: api_url,
+    //             data: {
+    //                 name: $('#searchbar').val().toLowerCase()
+    //             },
+    //         });
+    //     },
+    // });
+}); 
