1dfda2e5877a996f3e4eb1722aecab7d02ed626e
MariaLinhares <marialuis.linhares10@gmail.com>
2022-12-28 14:07:30 +0000

    fixed cards

--- myScript.js
+++ myScript.js
@@ -2,4 +2,18 @@ $('document').ready(function () {
     const carousel = new bootstrap.Carousel('#myCarousel', {
         interval: 10000
     });
+
+
+    const api_url = "http://192.168.160.58/Olympics/Help"
+
+    $("#searchbar").autocomplete({
+        minLenght: 3,
+        source: function (request, response) {
+            $.ajax({
+                type: "GET",
+                url: api_url,
+                data: {
+                    name: $('#searchbar').val().toLowerCase()
+                },
+    });
 });
