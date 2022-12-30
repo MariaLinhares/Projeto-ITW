193f0aaf990b0a70f62a14013ca9ca57633cbdf4
MariaLinhares <marialuis.linhares10@gmail.com>
2022-12-30 15:54:45 +0000

    default image athletes, favourites

--- athletes.js
+++ athletes.js
@@ -59,6 +59,38 @@ var vm = function () {
             self.totalRecords(data.TotalRecords);
             //self.SetFavourites();
         });
+        self.favourites = {
+            athletes: [],
+            games: [],
+            }; 
+        
+            self.loadFavourites = function(){
+                if (localStorage.getItem('favourites') != null){
+                    self.favourites = JSON.parse(localStorage.favourites)
+                } else {
+                    localStorage.setItem('favourites', JSON.stringify(self.favourites));
+                };
+        
+                let Favoritos = self.favourites.athletes;
+        
+                Favoritos.forEach(id => {
+                    $("#favourite_"+id).css('color','red');
+                });
+            }
+        
+            self.updateFavourites = function(id){
+                let index = Favoritos.indexOf(String(id))
+                if(index !== -1){
+                    $("#favourite_"+id).css('color', '#333')
+                    Favoritos.splice(index, 1)
+                } else if(index == -1){
+                    $("#favourite_"+id).css('color', 'red')
+                    Favoritos.push(String(id))
+                };
+                console.log(Favoritos)
+                console.log(self.favourites);
+                window.localStorage.setItem('favourites', JSON.stringify(self.favourites))
+            };
     };
 
     //--- Internal functions
@@ -130,3 +162,4 @@ $(document).ready(function () {
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
 })
+
