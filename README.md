75c408c1dc0e1dc860713c7c5c8b4a395465a9a8
MariaLinhares <marialuis.linhares10@gmail.com>
2023-01-03 05:34:03 +0000

    opa já nem sei

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Projeto-ITW
-Projeto final de ITW
-Site sobre os Jogos Olimpicos
+Este trabalho contém os seguintes componentes:
+
+-> Navbar
+-> Autocomplete
+-> Cards
+-> Modal
+-> Navegação por página
+-> LocalStorage para os favoritos
+-> Carrossel
+-> Font Awesome para a navbar
+-> Imagens dos atletas, competições, paises, jogos e modalidades (os que não têm imagem ficam com uma default)
+-> Pesquisa com selectMenu(jQuery UI) por tipo
+-> Formulário de sugestões 
+-> Tabela para guardar os dados inseridos no formulário que posteriormente ficam guardados no LocalStorage
+-> Footer
+-> Gráficos para mostrar algumas estatísticas da API (Google Charts)
+-> Nos favoritos aparecem todas as categorias que o utilizador adicionou aos favoritos onde podem ser Sortable (jQuery UI)
--- favs.js
+++ favs.js
@@ -4,7 +4,77 @@ function favs() {
     self.athleteData = ko.observable();
 
     self.athleteData = ko.observableArray([]);
-    self.favourites = {
+    self.favouritesData = {
         athletes: []
+        //falta restantes categorias
     };
-};
+
+    self.athleteFav = ko.observableArray([]);
+    //falta restantes categorias
+
+
+
+    self.updateLocalStorage = (key, data) => {
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+
+    function findImage(id) {
+        for (let i = 0; i< IMAGES.length;i++){
+            if (IMAGES[i].id === id){
+                if (IMAGES[i].img_url === "none"){
+                    return "./img/questioningMark.png"
+                }else {
+                    return IMAGES[i].img_url
+                }
+            }
+        }
+        return "./img/questioningMark.png"
+    }
+
+    self.loadData = function (array) { 
+        let temp = []
+        for (e in array) {
+            $.ajax({
+                type: "GET",
+                url: "http://192.168.160.58/Olympics/api/athletes" ,
+                async: false,
+                success: function (data) {
+                    data.img = findImage(data.Id)
+                    temp.push(data)
+                }
+            });
+        }
+        return temp
+    }
+    
+    self.updateFavouritesData = function (id, name) {
+        if (!$('#' + name + '-button').hasClass("active")) {
+            //Adicionar à lista de favoritos
+            if (!self.favouritesData[name].includes(id))
+            self.favouritesData[name].push(String(id))
+
+            self.updateLocalStorage(name, self.favouritesData[name])
+            $('#' + name + '-button').addClass("active")
+        } else {
+            //Remover do favoritos
+            self.favouritesData[name].splice(self.favouritesData[name].indexOf(id), 1)
+            self.updateLocalStorage(name, self.favouritesData[name])
+
+            $('#' + name + '-button').removeClass("active")
+        }
+        self.athleteFav(self.loadData(self.favouritesData.watchLater))
+    }
+
+    self.init = function () {
+        for (let k in self.favouritesData) {
+            if (localStorage.getItem(k) != null) {
+                self.favouritesData[k] = JSON.parse(localStorage.getItem(k))
+            } else {
+                self.favouritesData[k] = []
+            }
+        }
+
+        self.athleteFav(self.loadData(self.favouritesData.watchLater))
+
+    self.init()
+}};
