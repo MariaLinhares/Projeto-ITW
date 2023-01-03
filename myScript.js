68c78f6bfe4df2f245c830d7a84c779c0c97449d
MariaLinhares <marialuis.linhares10@gmail.com>
2023-01-03 09:26:05 +0000

    sla

--- myScript.js
+++ myScript.js
@@ -2,20 +2,4 @@ $('document').ready(function () {
     const carousel = new bootstrap.Carousel('#myCarousel', {
         interval: 10000
     });
-
-
-    const api_url = "http://192.168.160.58/Olympics/Help"
-
-    // $("#searchbar").autocomplete({
-    //     minLenght: 3,
-    //     source: function (request, response) {
-    //         $.ajax({
-    //             type: "GET",
-    //             url: api_url,
-    //             data: {
-    //                 name: $('#searchbar').val().toLowerCase()
-    //             },
-    //         });
-    //     },
-    // });
-}); 
+});
